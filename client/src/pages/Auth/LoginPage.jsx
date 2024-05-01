b73a38261d907077aa0d46c3de912166c4ed6323
Victor Raul <v1c70rotakmer@gmail.com>
2024-05-01 18:32:32 -0500

    Update client-side code with new components and routes

--- client/src/pages/Auth/LoginPage.jsx
+++ client/src/pages/Auth/LoginPage.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
 import { useDispatch } from 'react-redux';
 import { setUser } from '../../store/slices/userSlice';
-import { useNavigate } from 'react-router-dom';
 import AuthLayout from '../../layouts/AuthLayout';
-import { useState } from 'react';
-import { UserIcon, LockIcon } from '../../assets/icons';
 
+import { UserIcon, LockIcon } from '../../assets/icons';
+import { LogoGoogle } from '../../assets/images';
+import { useAuth0 } from '@auth0/auth0-react';
 const LoginPage = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -14,45 +17,83 @@ const LoginPage = () => {
         dispatch(setUser({ name: 'John Doe' }));
         navigate('/dashboard');
     };
+    const { loginWithRedirect } = useAuth0();
     return (
         <AuthLayout>
-            <div className="flex flex-col items-center justify-center">
-                <h1 className="text-3xl font-bold">
-                    Iniciar sesión en ODONTOLOGICA
-                </h1>
-                <form>
-                    <div className="flex justify-between items-center border-b-[4px] py-1 border-blue-400 rounded w-full text-gray-400 bg-white ring-transparent relative">
+            <div className="h-full flex flex-col justify-center w-full md:w-[80%] lg:w-[60%] max-w-[480px] mx-auto">
+                <header>
+                    <h2 className="text-3xl font-bold mb-10 text-left">
+                        Inicia sesión
+                    </h2>
+                </header>
+                <form className="flex flex-col gap-y-9 w-full">
+                    <label
+                        htmlFor="email"
+                        className="input-form-auth-container"
+                    >
                         <img
                             src={UserIcon}
                             alt="icono de usuario"
-                            className="size-8"
+                            className="size-7"
                         />
                         <input
                             type="email"
                             id="email"
                             placeholder="Correo"
-                            className="px-2"
+                            className="px-2 w-full bg-transparent"
                         />
-                    </div>
-                    <div className="border-b-[4px] border-blue-400 px-2 py-1 rounded w-full text-gray-400 bg-white ring-transparent relative">
+                    </label>
+                    <label
+                        htmlFor="password"
+                        className="input-form-auth-container"
+                    >
                         <img
                             src={LockIcon}
                             alt="candado icono"
-                            className="size-8"
+                            className="size-7"
                         />
                         <input
                             type="password"
                             id="password"
                             placeholder="Contraseña"
-                            className="ring-transparent border-none"
+                            className="px-2 w-full bg-transparent"
                         />
+                    </label>
+                    <div className="flex items-center justify-center gap-x-3">
+                        <a href="/" className="text-center text-primary flex-1">
+                            ¿Olvidaste tu contraseña?{' '}
+                        </a>
+                        <button
+                            type="button"
+                            onClick={handleClickLogin}
+                            className="w-full flex-1 bg-blue-500 text-white px-4 py-2 rounded-vm font-semibold"
+                            disabled={isLoading}
+                        >
+                            {isLoading ? 'Cargando...' : 'Iniciar sesión'}
+                        </button>
                     </div>
-                    <button
-                        type="button"
-                        onClick={handleClickLogin}
-                        className="bg-blue-500 text-white px-4 py-2 rounded"
-                        disabled={isLoading}
-                    />
+
+                    <div className="flex justify-center">
+                        <button
+                            type="button"
+                            className="w-full flex px-4 py-1 rounded-vm gap-x-2 justify-center items-center text-gray-800 border-[1px] border-slate-200 font-semibold"
+                            onClick={() => loginWithRedirect()}
+                        >
+                            <img
+                                src={LogoGoogle}
+                                alt="Logo de Google"
+                                className="size-8"
+                            />
+                            <span>Iniciar sesión con Google</span>
+                        </button>
+                    </div>
+                    <hr className="border-t border-slate-200" />
+                    <p className="text-center text-gray-800">
+                        ¿No tienes una cuenta?{' '}
+                        <Link to="/register" className="text-primary font-bold">
+                            Regístrate
+                        </Link>
+                    </p>
                 </form>
             </div>
         </AuthLayout>
--- client/src/pages/Dashboard/DashboardPage.jsx
+++ client/src/pages/Dashboard/DashboardPage.jsx
@@ -1,12 +1,16 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import { useSelector } from 'react-redux';
 
 const DashboardPage = () => {
-    const { user } = useSelector((state) => state.user);
-    console.log(user);
+    // const { user } = useSelector((state) => state.user);
+    const { user, isAuthenticated, isLoading } = useAuth0();
+    console.log('user:', user);
+    console.log('auth:', isAuthenticated);
     return (
         <section>
             <h2>Dashboard</h2>
-            <p>Welcome, {user.name}</p>
+            <p>Welcome, {user?.name}</p>
+            {!isAuthenticated && !isLoading && <p>Not logged in</p>}
         </section>
     );
 };

5830870e7281d88e385bc1a451d473d1fc56a77d
Victor Raul <v1c70rotakmer@gmail.com>
2024-05-01 21:18:45 -0500

    Update JWT_SECRET import paths in validate-jwt.js and createToken.js

--- server/src/middlewares/validate-jwt.js
+++ server/src/middlewares/validate-jwt.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../config/env.config';
+import { JWT_SECRET } from '../config/env.config.js';
 
 export const validateJWT = (req, res, next) => {
     const token = req.header('Authorization');
--- server/src/utils/createToken.js
+++ server/src/utils/createToken.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../config/env.config';
+import { JWT_SECRET } from '../config/env.config.js';
 
 export const createToken = (user) => {
     return jwt.sign(user, JWT_SECRET, { expiresIn: '8h' });

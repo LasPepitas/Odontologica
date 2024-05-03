cf8fa4151bf924b6e0ac3926f84fd94ce6de8710
Victor Raul <v1c70rotakmer@gmail.com>
2024-05-02 22:49:07 -0500

    Update baseURL in app.js to use development URL

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ const config = {
     authRequired: false,
     auth0Logout: true,
     secret: 'MWqCXqFwB7vJMouJRwg0N-sK2kVfTRYfYo4PU9YTcFiza22G3zp9TMeAGfk5Z9tX',
-    baseURL: 'http://odontologica.onrender.com',
+    baseURL: 'http://localhost:3000',
     clientID: 'af2OjaXew9TDVBvoolXMo1LkL19qD0Si',
     issuerBaseURL: 'https://valece.us.auth0.com',
 };
@@ -22,8 +22,16 @@ app.use(auth(config));
 app.get('/', (req, res) => {
     // Verifica si el usuario está autenticado
     if (req.oidc.isAuthenticated()) {
-        // Redirige al usuario a tu página web después de la autenticación
-        res.redirect('https://odontologica.pages.dev');
+        // Obtén los datos del usuario
+        const usuario = req.oidc.user;
+        // Codifica los datos del usuario en formato JSON y base64
+        const usuarioCodificado = Buffer.from(JSON.stringify(usuario)).toString(
+            'base64',
+        );
+        // Redirige al usuario a tu página web después de la autenticación y pasa los datos del usuario como parámetro de consulta en la URL
+        res.redirect(
+            `https://odontologica.pages.dev/login?usuario=${usuarioCodificado}`,
+        );
     } else {
         // Si no está autenticado, muestra un mensaje de bienvenida
         res.send('Hola mundo!');

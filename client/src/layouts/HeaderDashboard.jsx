9bb51ce3c104efa1804dca397de8b46a6b1c5232
Victor Raul <v1c70rotakmer@gmail.com>
2024-05-02 23:35:15 -0500

    chore: Update dependencies and add module type to package.json

--- client/src/layouts/HeaderDashboard.jsx
+++ client/src/layouts/HeaderDashboard.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MenuIcon } from '../assets/icons';
+import Options from './Options';
 
-const HeaderDashboard = () => {
+const HeaderDashboard = ({showMenu}) => {
+    
     return (
-        <div className="w-full h-[10%] bg-[#184D61] flex items-center justify-between px-4">
+        <div className="w-full h-[10%] bg-[#184D61] flex items-center justify-between px-4 relative">
+            {/* Botón del menú */}
+            <button className='md:hidden' onClick={showMenu}>
+                <img
+                    src={MenuIcon}
+                    alt="menu icono"
+                    className="size-7 mx-3"
+                />
+            </button>
             <div className='w-60 h-10 bg-[#D9D9D9] '>
                 LOGO DE LA EMPRESA
             </div>
-            <div>
+            {/* Botón de salida */}
+            <div className="relative">
                 <button className="bg-[#11728D] text-white my-2 px-4 py-2 rounded-lg">Salir</button>
             </div>
         </div>
--- client/src/pages/Dashboard/DashboardPage.jsx
+++ client/src/pages/Dashboard/DashboardPage.jsx
@@ -2,18 +2,30 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { useSelector } from 'react-redux';
 import SideNav from '../../layouts/SideNav'
 import HeaderDashboard from '../../layouts/HeaderDashboard';
+import { useState } from 'react';
+import Options from '../../layouts/Options';
 
 const DashboardPage = () => {
     const { user } = useSelector((state) => state.user);
     const { user: userGoogle, isAuthenticated, isLoading } = useAuth0();
-    console.log('userGoogle:', userGoogle);
-    console.log('auth:', isAuthenticated);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <div className='w-full h-full flex flex-col'>
-            <HeaderDashboard></HeaderDashboard>
-            <div className='w-full h-[655px] flex bg-[#D9D9D9] flex-row'>
-                <SideNav name={user?.name}></SideNav>
-                <div className='w-[70%]'>
+        <div className='w-full h-screen flex flex-col font-imprima'>
+            <HeaderDashboard showMenu={toggleMenu}></HeaderDashboard>
+            <div className='w-full h-full md:flex bg-[#D9D9D9] flex-row relative'>
+                <div className='hidden md:flex'>
+                    <SideNav name={user?.name}></SideNav>
+                </div>
+                <div className='md:w-[70%] w-full h-full relative'>
+                    {isMenuOpen && <div className="absolute inset-0 bg-black bg-opacity-30 backdrop-blur-lg z-10"></div>}
+                    <div className="absolute inset-0 flex justify-center items-center z-20">
+                        {isMenuOpen && <Options className="bg-white shadow-lg rounded-md"/>}
+                    </div>
                     {!isAuthenticated && !isLoading && <p>Not logged in</p>}
                 </div>
             </div>

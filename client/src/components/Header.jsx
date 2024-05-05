cf8a9c3c6a12e0fac484cd66515845a2b87ca91a
JSEDUARDO997 <jeduardomaquera@gmail.com>
2024-05-04 20:38:19 -0500

    agregando Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,61 +19,41 @@ export default function Header() {
               <ul className="flex items-center gap-6 text-sm">
                 <li>
                   <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
+                    className="text-black transition hover:text-[#0c8fae]"
                     href="#"
                   >
                     {" "}
-                    About{" "}
+                    Home{" "}
                   </a>
                 </li>
 
                 <li>
                   <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
+                    className="text-black transition hover:text-[#0c8fae]"
                     href="#"
                   >
                     {" "}
-                    Careers{" "}
+                    Doctores{" "}
                   </a>
                 </li>
 
                 <li>
                   <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
+                    className="text-black transition hover:text-[#0c8fae]"
                     href="#"
                   >
                     {" "}
-                    History{" "}
+                    Servicios{" "}
                   </a>
                 </li>
 
                 <li>
                   <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
+                    className="text-black transition hover:text-[#0c8fae]"
                     href="#"
                   >
                     {" "}
-                    Services{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    Projects{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    Blog{" "}
+                    Contacto{" "}
                   </a>
                 </li>
               </ul>
@@ -83,39 +63,11 @@ export default function Header() {
           <div className="flex items-center gap-4">
             <div className="sm:flex sm:gap-4">
               <a
-                className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+                className="rounded-md bg-[#0c8fae] px-5 py-2.5 text-sm font-medium text-white shadow"
                 href="#"
               >
-                Login
+                Home
               </a>
-
-              <div className="hidden sm:flex">
-                <a
-                  className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
-                  href="#"
-                >
-                  Register
-                </a>
-              </div>
-            </div>
-
-            <div className="block md:hidden">
-              <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              </button>
             </div>
           </div>
         </div>

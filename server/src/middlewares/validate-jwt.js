6aaf226606ded0a92716e0b9f42021b26dec1866
Victor Raul <v1c70rotakmer@gmail.com>
2024-05-10 16:25:10 -0500

    chore: Update validateJWT middleware to extract token from Authorization header

--- server/src/middlewares/validate-jwt.js
+++ server/src/middlewares/validate-jwt.js
@@ -2,8 +2,8 @@ import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '../config/env.config.js';
 
 export const validateJWT = (req, res, next) => {
-    const token =
-        req.header('Authorization') || req.header('x-access-token') || null;
+    const token = req.header('Authorization').split(' ')[1];
+    console.log(token);
     if (!token) {
         return res.status(401).json({ error: 'Unauthorized' });
     }

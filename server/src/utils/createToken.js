87cd9630de9e7099afe5ae6849b7ab4793403941
Victor Raul <v1c70rotakmer@gmail.com>
2024-05-12 16:21:25 -0500

    fix

--- server/src/utils/createToken.js
+++
@@ -1,6 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../config/env.config.js';
-
-export const createToken = (user) => {
-    return jwt.sign(user, JWT_SECRET, { expiresIn: '8h' });
-};
--- server/src/utils/uploadImage.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-import { configCloudinary } from '../config/env.config.js';
-
-cloudinary.config(configCloudinary);
-export async function uploadImage(filePath) {
-    return await cloudinary.uploader.upload(filePath, {
-        folder: 'app-db',
-    });
-}

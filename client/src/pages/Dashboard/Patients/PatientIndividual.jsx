4e2640ee4d3fad5ba6b74f9dcbe36f34879e3da9
Victor Raul <v1c70rotakmer@gmail.com>
2024-05-23 20:19:42 -0500

    chore: Refactor PatientIndividual component to use table for displaying patient information

--- client/src/pages/Dashboard/Patients/PatientIndividual.jsx
+++ client/src/pages/Dashboard/Patients/PatientIndividual.jsx
@@ -1,9 +1,11 @@
 import { useParams } from 'react-router-dom';
 import { getAppointmentById } from '../../../services';
 import { useEffect, useState } from 'react';
+
 const PatientIndividual = () => {
     const { id } = useParams();
     const [appointment, setAppointment] = useState({});
+
     useEffect(() => {
         getAppointmentById(id).then((data) =>
             setAppointment({
@@ -12,71 +14,102 @@ const PatientIndividual = () => {
             }),
         );
     }, [id]);
+
     const handlePayment = () => {
         console.log('Pago confirmado');
     };
+
     console.log(appointment);
+
     return (
-        <div>
+        <div className="p-5">
             <h2 className="font-bold text-2xl mb-5">
                 Información del paciente
             </h2>
-            <div>
-                <div>
-                    <h3 className="font-bold text-xl">Nombre:</h3>
-                    <p>{appointment.user_name}</p>
-                </div>
-                <div>
-                    <h3 className="font-bold text-xl">Apellido:</h3>
-                    <p>{appointment.user_lastName ?? 'No disponible'}</p>
-                </div>
-                <div>
-                    <h3 className="font-bold text-xl">Email:</h3>
-                    <p>{appointment.user_email}</p>
-                </div>
-                <div>
-                    <h3 className="font-bold text-xl">Estado de la cita:</h3>
-                    <p>{appointment.status}</p>
-                </div>
-                <div>
-                    <h3 className="font-bold text-xl">Fecha:</h3>
-                    <p>{appointment.date}</p>
-                </div>
-                <div>
-                    <h3 className="font-bold text-xl">Duración:</h3>
-                    <p>{appointment.duration}</p>
-                </div>
-                <div>
-                    <h3 className="font-bold text-xl">Descripción:</h3>
-                    <p>{appointment.description}</p>
-                </div>
-                <div>
-                    <h3 className="font-bold text-xl">Método de pago:</h3>
-                    <p>{appointment.payment_method}</p>
-                </div>
-                <div>
-                    <h3 className="font-bold text-xl">Monto:</h3>
-                    <p>{appointment.amount}</p>
-                </div>
-                <div>
-                    <h3 className="font-bold text-xl">Estado de pago:</h3>
-                    <p>{appointment.status}</p>
-                </div>
-                <div>
-                    <h3 className="font-bold text-xl">Recibo:</h3>
-                    {appointment.payment_receipt_image ? (
-                        <img
-                            src={appointment.payment_receipt_image}
-                            alt="Recibo de pago"
-                            className="w-1/2"
-                        />
-                    ) : (
-                        <p className="py-6">No hay recibo</p>
-                    )}
-                </div>
+            <table className="min-w-full divide-y divide-gray-200">
+                <thead>
+                    <tr>
+                        <th className="font-bold text-xl p-2 text-left">
+                            Campo
+                        </th>
+                        <th className="font-bold text-xl p-2 text-left">
+                            Valor
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="font-bold text-xl p-2">Nombre:</td>
+                        <td className="p-2">
+                            {appointment.user_name || 'No disponible'}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold text-xl p-2">Apellido:</td>
+                        <td className="p-2">
+                            {appointment.user_lastName ?? 'No disponible'}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold text-xl p-2">Email:</td>
+                        <td className="p-2">
+                            {appointment.user_email || 'No disponible'}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold text-xl p-2">
+                            Estado de la cita:
+                        </td>
+                        <td className="p-2">{appointment.status}</td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold text-xl p-2">Fecha:</td>
+                        <td className="p-2">{appointment.date}</td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold text-xl p-2">Duración:</td>
+                        <td className="p-2">{appointment.duration}</td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold text-xl p-2">Descripción:</td>
+                        <td className="p-2">{appointment.description}</td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold text-xl p-2">
+                            Método de pago:
+                        </td>
+                        <td className="p-2">{appointment.payment_method}</td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold text-xl p-2">Monto:</td>
+                        <td className="p-2">{'S/. ' + appointment.amount}</td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold text-xl p-2">
+                            Estado de pago:
+                        </td>
+                        <td className="p-2">{appointment.payment_status}</td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold text-xl p-2">Recibo:</td>
+                        <td className="p-2">
+                            {appointment.payment_receipt_image ? (
+                                <img
+                                    src={appointment.payment_receipt_image}
+                                    alt="Recibo de pago"
+                                    className="w-[90%] md:w-[250px]"
+                                />
+                            ) : (
+                                <p className="py-6">No hay recibo</p>
+                            )}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div className="mt-5 w-full flex justify-center">
                 <button
                     onClick={handlePayment}
-                    className="bg-blue-500 text-white py-2 px-6 w-fit rounded-md"
+                    className="bg-blue-500 text-white py-2 px-8 w-fit rounded-md font-bold"
                 >
                     Confirmar pago
                 </button>
